9acbd356e5729686669e28ed908de234ce517e7a
Andrey Suvorov <suvorov.andrey755@gmail.com>
2025-12-05 04:03:57 +0300

    add func get size

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	pathSize "code"
 	"context"
 	"fmt"
 	"os"
@@ -12,6 +13,18 @@ func main() {
 	app := &cli.Command{
 		Name: "hexlet-path-size",
 		Usage: "print size of a file or directory",
+		Action: func(_ context.Context, ctx *cli.Command) error {
+			if ctx.Args().Len() != 1 {
+				return fmt.Errorf("usage: hexlet-path-size <path>")
+			}
+			path := ctx.Args().Get(0)
+			size, err := pathSize.GetSize(path)
+			if err != nil {
+				return err
+			}
+			fmt.Println(size)
+			return nil
+		},
 	}
 
 	if err := app.Run(context.Background(), os.Args); err != nil {
--- path_size.go
+++ path_size.go
@@ -1,2 +1,38 @@
 package pathSize
 
+import (
+	"fmt"
+	"os"
+)
+
+func GetSize(path string) (string, error) {
+
+	info, err := os.Lstat(path)
+	if err != nil {
+		return "", err
+	}
+
+	if info.IsDir() {
+		return fmt.Sprintf("%dB\t%s", info.Size(), path), nil
+	}
+
+	value, err := os.ReadDir(path)
+	
+	if err != nil {
+		return "", err
+	}
+	var total int64
+
+	for _, v := range value {
+		if v.Type().IsRegular() {
+			info, err := v.Info()
+
+			if err != nil {
+				continue
+			}
+			total += info.Size()
+		}
+	}
+
+	return fmt.Sprintf("%dB\t%s", total, path), nil
+}
